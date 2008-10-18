a8c15ecf45f3408a04e2f3a3cf1e38d6e3144802
Steven Noonan <steven@uplinklabs.net>
2008-10-17 20:35:37 -0700

    clean.sh: better git/svn detection, and cleaner output
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- clean.sh
+++ clean.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-echo
-echo "The following files will be purged:"
-if [ -d .svn ]; then
-	svn status --no-ignore | grep ^[I?] | sed 's/^[I?]//g' | sed 's/^[ ].//g'
-else
-	git clean -n -f -x -d
+# Quick sanity check.
+if [ ! -d .svn ]; then
+	if [ ! -d .git ]; then
+		echo "This project doesn't seem to be under source control."
+		exit 1
+	fi
 fi
 
 echo
-read -p "Continue (Y/N)? "
+if [ -d .svn ]; then
+	svn status --no-ignore | grep ^[I?] | sed 's/^[I?]//g' | sed 's/^[ ].//g'
+else
+	if [ -d .git ]; then
+		git clean -nfxd
+	fi
+fi
+echo
+
+read -p "Remove the above listed files (Y/N)? "
+echo
 if [ "$REPLY" == "y" ]; then
 	if [ -d .svn ]; then
 		rm -rvf `svn status --no-ignore | grep ^[I?] | sed 's/^[I?]//g' | sed 's/^[ ].//g'`
 	else
-		git clean -f -x -d
+		git clean -fxd
 	fi
 else
 	echo
