c8ba8d7ce2962e39d5a329a96d7747c7dfaf6551
Steven Noonan <steven@uplinklabs.net>
2009-01-22 15:54:54 -0800

    universal_include: add GCC extension support Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- source/crisscross/universal_include.h
+++ source/crisscross/universal_include.h
@@ -247,7 +247,35 @@ typedef long intptr_t;
 #include <sstream>
 #endif
 
-__inline char *cc_strdup(const char *x)
+#if __GNUC__ >= 3
+#define inline         inline __attribute__ ((always_inline))
+#define __pure         __attribute__ ((pure))
+#define __const        __attribute__ ((const))
+#define __noreturn     __attribute__ ((noreturn))
+#define __malloc       __attribute__ ((malloc))
+#define __must_check   __attribute__ ((warn_unused_result))
+#define __deprecated   __attribute__ ((deprecated))
+#define __used         __attribute__ ((used))
+#define __unused       __attribute__ ((unused))
+#define __packed       __attribute__ ((packed))
+#define likely(x)      __builtin_expect(!!(x), 1)
+#define unlikely(x)    __builtin_expect(!!(x), 0)
+#else
+#define inline         /* no inline */
+#define __pure         /* no pure */
+#define __const        /* no const */
+#define __noreturn     /* no noreturn */
+#define __malloc       /* no malloc */
+#define __must_check   /* no warn_unused_result */
+#define __deprecated   /* no deprecated */
+#define __used         /* no used */
+#define __unused       /* no unused */
+#define __packed       /* no packed */
+#define likely(x)      (x)
+#define unlikely(x)    (x)
+#endif
+
+inline char *cc_strdup(const char *x)
 {
 	if (!x) return NULL;
 
