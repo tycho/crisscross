ce276933efcac866f68d45d613f4a44c15e21488
Steven Noonan <steven@uplinklabs.net>
2009-01-27 00:30:37 -0800

    TestSuite: print file and line number in assertion
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- TestSuite/testutils.h
+++ TestSuite/testutils.h
@@ -20,7 +20,8 @@ extern CrissCross::IO::Console *g_console;
 #define TEST_ASSERT(condition) \
         if (!(condition)) { \
 		g_console->SetColour(g_console->FG_YELLOW | g_console->FG_INTENSITY); \
-		g_console->WriteLine("\nWARNING: Condition '%s' failed assertion!", # condition); \
+		g_console->WriteLine("\nWARNING: Condition '%s' failed assertion!\nFile: '%s' Line: %d", \
+			#condition, __FILE__, __LINE__); \
 		g_console->SetColour(g_console->FG_RED | g_console->FG_INTENSITY); \
 		CrissCross::Debug::PrintStackTrace(g_console); \
 		g_console->SetColour(); \
