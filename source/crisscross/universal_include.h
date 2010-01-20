232c8b3c18d0faf8fdaef7b750c76443bdfdf43d
Steven Noonan <steven@uplinklabs.net>
2010-01-19 16:14:11 -0800

    universal_include.h: fix definition if _WIN32_IE
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- source/crisscross/universal_include.h
+++ source/crisscross/universal_include.h
@@ -79,8 +79,8 @@
 #ifndef _WIN32_WINNT
 #define _WIN32_WINNT _WIN32_WINNT_WINXP
 #endif
-#ifndef _WIN32_IE_
-#define _WIN32_IE_   _WIN32_IE_IE70
+#ifndef _WIN32_IE
+#define _WIN32_IE   _WIN32_IE_IE70
 #endif
 #endif
 
