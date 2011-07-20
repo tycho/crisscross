d4e50333dc953cd2eeb91211246853b2a12057f8
Steven Noonan <steven@uplinklabs.net>
2011-07-20 06:13:55 +0000

    HashTable: now with 100% more stupidity
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- source/crisscross/hashtable.cpp
+++ source/crisscross/hashtable.cpp
@@ -17,109 +17,91 @@
 #include <crisscross/hash.h>
 #include <crisscross/internal_mem.h>
 
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+
+#ifdef TARGET_OS_MACOSX
+#define MAP_ANONYMOUS MAP_ANON
+#endif
+
 namespace CrissCross
 {
 	namespace Data
 	{
 		template <class Data>
-		HashTable<Data>::HashTable(size_t _initialSize)
-		: m_keys(NULL), m_size(32)
+		HashTable<Data>::HashTable(uint64_t _initialSize)
 		{
-			m_mask = m_size - 1;
+			/* This many elements should be enough for anybody. */
+			m_size = ((uint32_t)-1) - 2; 
 			m_slotsFree = m_size;
-			m_keys = new char *[m_size];
-			m_data = new Data[m_size];
+			m_data = (kvpair_t *)mmap(NULL, m_size * sizeof(kvpair_t), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS , -1, 0);
 
-			memset(m_keys, 0, sizeof(char *) * m_size);
-			memset(m_data, 0, sizeof(Data) * m_size);
+			/* Assert that the memory was actually allocated. */
+			CoreAssert(m_data != (kvpair_t *)-1);
+
+			/* Assert that the default contents is zeroed. */
+			CoreAssert(*((uint64_t *)m_data) == 0);
 		}
 
 		template <class Data>
 		HashTable<Data>::~HashTable()
 		{
 			empty();
-
-			delete [] m_keys;
-			delete [] m_data;
+			munmap(m_data, m_size * sizeof(kvpair_t));
 		}
 
 		template <class Data>
 		void HashTable<Data>::grow()
 		{
-			unsigned int oldSize = m_size;
-			m_size *= 2;
-			m_mask = m_size - 1;
-			char **oldKeys = m_keys;
-			m_keys = new char *[m_size];
-			Data *oldData = m_data;
-			m_data = new Data[m_size];
-
-			memset(m_keys, 0, sizeof(char *) * m_size);
-			memset(m_data, 0, sizeof(Data) * m_size);
-
-			for (size_t i = 0; i < oldSize; ++i) {
-				if (oldKeys[i] != NULL) {
-					size_t newIndex = findInsertIndex(oldKeys[i]);
-					m_keys[newIndex] = oldKeys[i];
-					m_data[newIndex] = oldData[i];
-				}
-			}
-
-			m_slotsFree += m_size - oldSize;
-
-			delete [] oldKeys;
-			delete [] oldData;
+			/* No. I refuse. */
+			uint64_t YourHashTableIsTooDamnBig = 0;
+			CoreAssert(YourHashTableIsTooDamnBig);
 		}
 
 		template <class Data>
-		size_t HashTable<Data>::findInsertIndex(const char *_key) const
+		uint64_t HashTable<Data>::findInsertIndex(const char *_key) const
 		{
-			size_t index = Hash<const char *>(_key) & m_mask;
+			uint64_t index = Hash<const char *>(_key) % m_size;
 
-			while (m_keys[index] != NULL &&
-			       m_keys[index] != (char *)-1)
+			while (m_data[index].key != NULL)
 			{
-				CoreAssert(Compare<const char *>((const char *)m_keys[index], _key) != 0);
+				CoreAssert(Compare<const char *>((const char *)m_data[index].key, _key) != 0);
 				index++;
-				index &= m_mask;
+				if (index >= m_size)
+					index = 0;
 			}
 
 			return index;
 		}
 
 		template <class Data>
-		size_t HashTable<Data>::findIndex(const char *_key) const
+		uint64_t HashTable<Data>::findIndex(const char *_key) const
 		{
-			size_t index = Hash<const char *>(_key) & m_mask;
+			uint64_t index = Hash<const char *>(_key) % m_size;
+			kvpair_t *kvpair = &m_data[index];
 
-			if (m_keys[index] == NULL) {
-				return -1;
-			}
-
-			while (true)
-			{
-				if (m_keys[index] != NULL && m_keys[index] != (char*)-1) {
-					if (Compare<const char *>((const char *)m_keys[index], _key) == 0)
-						break;
-				}
+			while (kvpair->key) {
+				if (kvpair->key != (char *)-1 &&
+				    Compare<const char *>(kvpair->key, _key) == 0)
+					return index;
 
 				index++;
-				index &= m_mask;
+				if (index >= m_size)
+					index = 0;
 
-				if (m_keys[index] == NULL) {
-					return -1;
-				}
+				kvpair = &m_data[index];
 			}
 
-			return index;
+			return -1;
 		}
 
 		template <class Data>
 		Data HashTable<Data>::find(const char * _key, Data const &_default) const
 		{
-			size_t index = findIndex(_key);
-			if (index != (size_t)-1) {
-				return m_data[index];
+			uint64_t index = findIndex(_key);
+			if (index != (uint64_t)-1) {
+				return m_data[index].data;
 			}
 			return _default;
 		}
@@ -133,11 +115,11 @@ namespace CrissCross
 		template <class Data>
 		bool HashTable<Data>::erase(const char *_key)
 		{
-			size_t index = findIndex(_key);
-			if (index != (size_t)-1) {
-				Dealloc(m_keys[index]);
-				m_keys[index] = (char*)-1;
-				m_slotsFree++;
+			uint64_t index = findIndex(_key);
+			if (index != (uint64_t)-1) {
+				Dealloc(m_data[index].key);
+				m_data[index].key = (char *)-1;
+				m_usedIndices.erase(index);
 				return true;
 			}
 			return false;
@@ -146,211 +128,35 @@ namespace CrissCross
 		template <class Data>
 		void HashTable<Data>::empty()
 		{
-			for (size_t i = 0; i < m_size; ++i) {
-				if (m_keys[i] == (char*)-1) m_keys[i] = NULL;
-				Dealloc(m_keys[i]);
+			std::set<uint64_t>::iterator it;
+			for (it = m_usedIndices.begin();
+			     it != m_usedIndices.end();
+				 it++)
+			{
+				kvpair_t *kvpair = &m_data[*it];
+				if (kvpair->key != NULL &&
+				    kvpair->key != (char *)-1)
+					Dealloc(kvpair->key);
+				kvpair->key = NULL;
 			}
-
-			memset(m_keys, 0, sizeof(char *) * m_size);
-			memset(m_data, 0, sizeof(Data) * m_size);
 		}
 
 		template <class Data>
-		size_t HashTable<Data>::insert(const char *_key, Data const &_data)
+		uint64_t HashTable<Data>::insert(const char *_key, Data const &_data)
 		{
-			if (m_slotsFree * 2 <= m_size) {
+			if (m_slotsFree < m_size / 4) {
 				grow();
 			}
 
-			size_t index = findInsertIndex(_key);
-			CoreAssert(m_keys[index] == NULL || m_keys[index] == (char*)-1);
-			m_keys[index] = cc_strdup(_key);
-			m_data[index] = _data;
+			uint64_t index = findInsertIndex(_key);
+			CoreAssert(m_data[index].key == NULL || m_data[index].key == (char*)-1);
+			m_data[index].key = cc_strdup(_key);
+			m_data[index].data = _data;
 			m_slotsFree--;
 
-			return index;
-		}
-
-		template <class T>
-		void SortingHashTable<T>::grow()
-		{
-			unsigned int oldSize = this->m_size;
-			this->m_size *= 2;
-			this->m_mask = (this->m_mask << 1) + 1;
-
-			/* Copy pointers to existing data */
-			char      * *oldKeys = this->m_keys;
-			T           *oldData = this->m_data;
-			size_t      *oldOrderedIndices = m_orderedIndices;
-
-			/* Make new data */
-			this->m_keys = new char *[this->m_size];
-			this->m_data = new T [this->m_size];
-			m_orderedIndices = new size_t[this->m_size];
-
-			/* Set all new data to zero */
-			memset(this->m_keys, 0, sizeof(char *) * this->m_size);
-			memset(this->m_data, 0, sizeof(T) * this->m_size);
-			for (size_t i = 0; i < this->m_size; i++)
-				m_orderedIndices[i] = (size_t)-1;
-
-			/* */
-			/* Go through the existing ordered index list, inserting elements into the */
-			/* new table as we go */
-
-			size_t oldI = m_firstOrderedIndex;
-			size_t newI = this->findInsertIndex(oldKeys[oldI]);
-			m_firstOrderedIndex = newI;
-			while (oldI != (size_t)-1)
-			{
-				size_t nextOldI = oldOrderedIndices[oldI];
-
-				this->m_keys[newI] = oldKeys[oldI];
-				this->m_data[newI] = oldData[oldI];
-
-				size_t nextNewI = (nextOldI != (size_t)-1) ? this->findInsertIndex(oldKeys[nextOldI]) : (size_t)-1;
-
-				m_orderedIndices[newI] = nextNewI;
-
-				oldI = nextOldI;
-				newI = nextNewI;
-			}
-
-			this->m_slotsFree += this->m_size - oldSize;
-
-			delete [] oldKeys;
-			delete [] oldData;
-			delete [] oldOrderedIndices;
-		}
-
-		/* See header for description */
-		template <class T>
-		size_t SortingHashTable<T>::findPrevKey(char const *_key) const
-		{
-			size_t prevI = -1;
-			size_t i = m_firstOrderedIndex;
-
-			while (1)
-			{
-				if (i == (size_t)-1) return prevI;
-
-				if (Compare((const char *)this->m_keys[i], _key) >= 0)
-					return prevI;
-
-				prevI = i;
-				i = m_orderedIndices[i];
-			}
-		}
-
-		template <class T>
-		SortingHashTable<T>::SortingHashTable() :
-			HashTable<T>(),
-			m_firstOrderedIndex(-1)
-		{
-			m_orderedIndices = new size_t[this->m_size];
-			memset(m_orderedIndices, 0, sizeof(size_t) * this->m_size);
-		}
-
-		template <class T>
-		SortingHashTable<T>::~SortingHashTable()
-		{
-			delete [] m_orderedIndices;
-		}
-
-		template <class T>
-		size_t SortingHashTable<T>::insert(char const *_key, T const &_data)
-		{
-			/* */
-			/* Make sure the table is big enough */
-
-			if (this->m_slotsFree * 2 <= this->m_size) {
-				grow();
-			}
-
-			/* */
-			/* Do the main insert */
-
-			size_t index = HashTable<T>::findInsertIndex(_key);
-			CoreAssert(this->m_keys[index] == NULL || this->m_keys[index] == (char*)-1);
-			this->m_keys[index] = cc_strdup(_key);
-			this->m_data[index] = _data;
-			this->m_slotsFree--;
-
-			/* */
-			/* Insert us into the alphabetically order index list */
-
-			size_t i = findPrevKey(_key);
-			if (i == (size_t)-1) {
-				/* Handle the case when the table is empty, or the new element is going */
-				/* to be the new first alphabetical element */
-				m_orderedIndices[index] = m_firstOrderedIndex;
-				m_firstOrderedIndex = index;
-			} else	{
-				m_orderedIndices[index] = m_orderedIndices[i];
-				m_orderedIndices[i] = index;
-			}
+			m_usedIndices.insert(index);
 
 			return index;
-		}
-
-		template <class T>
-		bool SortingHashTable<T>::erase(char const *_key)
-		{
-			int index = this->GetIndex(_key);
-			if (index >= 0)	{
-				return erase(index);
-			}
-			return false;
-		}
-
-		template <class T>
-		bool SortingHashTable<T>::erase(size_t _index)
-		{
-			bool retval = false;
-			/* */
-			/* Remove data */
-
-			if (this->m_keys[_index] != (char *)-1) {
-				retval = true;
-				delete [] this->m_keys[_index];
-			}
-			this->m_keys[_index] = (char *)-1;
-			this->m_slotsFree++;
-
-			/* */
-			/* Remove from ordered list */
-
-			short prevIndex = -1;
-			short index = m_firstOrderedIndex;
-			while (m_orderedIndices[index] != _index)
-			{
-				CoreAssert(index != -1);
-				prevIndex = index;
-				index = m_orderedIndices[index];
-			}
-
-			if (prevIndex == -1) {
-				m_firstOrderedIndex = -1;
-			} else	{
-				m_orderedIndices[prevIndex] = m_orderedIndices[index];
-			}
-			return retval;
-		}
-
-		template <class T>
-		size_t SortingHashTable<T>::beginOrderedWalk()
-		{
-			m_nextOrderedIndex = m_orderedIndices[m_firstOrderedIndex];
-			return m_firstOrderedIndex;
-		}
-
-		template <class T>
-		size_t SortingHashTable<T>::nextOrderedIndex()
-		{
-			size_t rv = m_nextOrderedIndex;
-			if (rv != (size_t)-1)
-				m_nextOrderedIndex = m_orderedIndices[m_nextOrderedIndex];
-			return rv;
 		}
 	}
 }
--- source/crisscross/hashtable.h
+++ source/crisscross/hashtable.h
@@ -12,42 +12,45 @@
 #ifndef __included_cc_hashtable_h
 #define __included_cc_hashtable_h
 
+#include <stdint.h>
+#include <set>
 #include <cstring>
 
 namespace CrissCross
 {
 	namespace Data
 	{
-		/*! \brief A simple HashTable. */
+		/*! \brief A hilariously stupid HashTable implementation. */
 		template <class Data>
 		class HashTable
 		{
 		protected:
-			char       **m_keys;
-			Data        *m_data;
-			unsigned int m_slotsFree;
-			unsigned int m_size;
-			unsigned int m_mask;
+			struct kvpair_t
+			{
+				char *key;
+				Data data;
+			};
+			kvpair_t *m_data;
+			uint64_t  m_size;
+			uint64_t  m_slotsFree;
+			std::set<uint64_t> m_usedIndices;
 
-			size_t findInsertIndex(const char *_key) const;
-			size_t findIndex(const char *_key) const;
+			uint64_t findInsertIndex(const char *_key) const;
+			uint64_t findIndex(const char *_key) const;
 			virtual void   grow();
 
 		public:
 			/*! \brief The constructor. */
-			/*!
-			 * \param _initialSize The initial size of the hash table. Minimum is 32.
-			 */
-			HashTable(size_t _initialSize = 32);
+			HashTable(uint64_t _ignored = 32);
 			virtual ~HashTable();
 
 			/*! \brief Inserts data into the table. */
 			/*!
 			 * \param _key The key of the data.
 			 * \param _data The data to insert.
-			 * \return True on success, false on failure.
+			 * \return Index of element in the table. 
 			 */
-			virtual size_t insert(const char *_key, Data const &_data);
+			virtual uint64_t insert(const char *_key, Data const &_data);
 
 			/*! \brief Finds a node in the table and returns the data at that node. */
 			/*!
@@ -71,7 +74,7 @@ namespace CrissCross
 			 * \param _index The index of the node to delete.
 			 * \return True on success, false on failure
 			 */
-			bool erase(size_t _index);
+			bool erase(uint64_t _index);
 
 			/*! \brief Tests whether a key is in the table or not. */
 			/*!
@@ -83,11 +86,11 @@ namespace CrissCross
 			/*! \brief Empties the table completely. */
 			void empty();
 
-			/*! \brief Indicates the number of subtrees. */
+			/*! \brief Indicates the maximum number of elements. */
 			/*!
-			 * \return Number of subtrees in the table.
+			 * \return Maximum number of elements the table can contain.
 			 */
-			inline size_t size() const
+			inline uint64_t size() const
 			{
 				return m_size;
 			}
@@ -96,120 +99,10 @@ namespace CrissCross
 			/*!
 			 * \return Number of items in the table.
 			 */
-			inline size_t used() const
+			inline uint64_t used() const
 			{
 				return m_size - m_slotsFree;
 			}
-
-			bool valid(size_t _index) const
-			{
-				CoreAssert(_index < m_size);
-				return m_keys[_index] != NULL &&
-				       m_keys[_index] != (char*)-1;
-			}
-
-			Data const &operator [](size_t _index) const
-			{
-				CoreAssert(_index < m_size);
-				return m_data[_index];
-			}
-
-			Data &operator [](size_t _index)
-			{
-				CoreAssert(_index < m_size);
-				return m_data[_index];
-			}
-
-#if !defined (DISABLE_DEPRECATED_CODE)
-			/*
-			 *      Deprecated Compatibility Functions
-			 *      Provided for compatibility with Tosser I
-			 */
-			/*! @cond */
-			int GetIndex(const char *_key) const
-			{
-				return (int)findIndex(_key);
-			}
-			int PutData(const char *_key, Data const &_data)
-			{
-				return (int)insert(_key, _data);
-			}
-			Data GetData(const char *_key, Data const &_default = NULL) const
-			{
-				return find(_key, _default);
-			}
-			Data GetData(unsigned int _index) const
-			{
-				return m_data[_index];
-			}
-			Data *GetPointer(const char *_key) const
-			{
-				int index = GetIndex(_key);
-				if (index >= 0)	{
-					return &m_data[index];
-				}
-				return NULL;
-			}
-			Data *GetPointer(unsigned int _index) const
-			{
-				return &m_data[_index];
-			}
-			void RemoveData(const char *_key)
-			{
-				erase(_key);
-			}
-			void RemoveData(unsigned int _index)
-			{
-			}
-			unsigned int Size() const { return (int)size(); }
-			unsigned int NumUsed() const { return (int)used(); }
-			bool ValidIndex(unsigned int _x) const { return valid(_x); }
-			char const *GetName(unsigned int _index) const { return m_keys[_index]; }
-			void Empty() { empty(); }
-			void EmptyAndDelete()
-			{
-				for (size_t i = 0; i < m_size; ++i) {
-					if (m_keys[i] != NULL && m_keys[i] != (char *)-1) {
-						delete m_data[i];
-					}
-				}
-				empty();
-			}
-			/*! @endcond */
-#endif
-		};
-
-		template <class Data>
-		class SortingHashTable : public HashTable<Data>
-		{
-		protected:
-			size_t	*m_orderedIndices;                    /* Stores a chain of indices that make it easy to walk through the table in alphabetical key order */
-			size_t	m_firstOrderedIndex;                  /* The index of the alphabetically first table element */
-			size_t	m_nextOrderedIndex;                   /* Used by nextOrderedIndex */
-
-			void	grow();
-			size_t	findPrevKey(char const *_key) const;  /* Returns the index of the table element whose key is alphabetically previous to the specified key */
-
-		public:
-			SortingHashTable();
-			~SortingHashTable();
-
-			size_t	insert(char const *_key, Data const &_data);
-			bool	erase(char const *_key);
-			bool	erase(size_t _index);
-
-			size_t	beginOrderedWalk();
-			size_t	nextOrderedIndex();
-#if !defined (DISABLE_DEPRECATED_CODE)
-			/*
-			 *      Deprecated Compatibility Functions
-			 *      Provided for compatibility with Tosser I
-			 */
-			/*! @cond */
-			size_t	StartOrderedWalk() { return beginOrderedWalk(); }
-			size_t	GetNextOrderedIndex() { return nextOrderedIndex(); }
-			/*! @endcond */
-#endif
 		};
 	}
 }
