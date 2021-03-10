7ce56d63b0070f46209df0f42879a6c269afdad9
Steven Noonan <steven@uplinklabs.net>
2021-03-10 14:01:34 -0800

    universal_include: use numeric constants for _WIN32_WINNT and _WIN32_IE
    And move them to something more recent.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- source/crisscross/universal_include.h
+++ source/crisscross/universal_include.h
@@ -46,10 +46,10 @@
 
 #if defined (TARGET_OS_WINDOWS)
 #ifndef _WIN32_WINNT
-#define _WIN32_WINNT _WIN32_WINNT_WINXP
+#define _WIN32_WINNT 0x0601
 #endif
 #ifndef _WIN32_IE
-#define _WIN32_IE   _WIN32_IE_IE70
+#define _WIN32_IE    0x0800
 #endif
 #if !defined(__SANITIZE_ADDRESS__)
 #ifdef _SCL_SECURE
