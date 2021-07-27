e9731522a5fd4f56ba7dd819e9cfde737825bbcf
Steven Noonan <steven@uplinklabs.net>
2021-07-26 20:57:19 -0700

    filesystem: move dirent/DIR definitions to header
    This makes these APIs actually usable...
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- source/crisscross/filesystem.h
+++ source/crisscross/filesystem.h
@@ -14,8 +14,30 @@
 
 #ifdef TARGET_COMPILER_VC
 
-struct dirent;
-struct DIR;
+#include <windows.h>
+
+typedef struct dirent {
+	/* name of current directory entry (a multi-byte character string) */
+	char d_name[MAX_PATH + 1];
+
+	/* file attributes */
+	WIN32_FIND_DATAA data;
+} dirent;
+
+
+typedef struct DIR {
+	/* current directory entry */
+	dirent current;
+
+	/* is there an un-processed entry in current? */
+	int cached;
+
+	/* file search handle */
+	HANDLE search_handle;
+
+	/* search pattern (3 = zero terminator + pattern "\\*") */
+	char patt[MAX_PATH + 3];
+} DIR;
 
 DIR *opendir(const char *dirname);
 struct dirent *readdir(DIR *dirp);
